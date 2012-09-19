cbd1d0201b1768ab5262ef1d355f2f0e51dec4a5
Frank Smith <oryx.gazella@gmail.com>
2012-09-19 22:36:07 +0200

    Add a test with a local stub of the desired ViewModel

---
+++ Ifkam.Viewmodels.Tests/MainPageModelTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace Ifkam.Viewmodels.Tests
+{
+    [TestFixture]
+    public class MainPageModelTests
+    {
+        [Test]
+        public void ShouldLookupWord()
+        {
+            //Arrange
+            var mainPageViewModel = new MainPageViewModel
+                                        {
+                                            Word = "look_me_up",
+                                            Definition = string.Empty
+                                        };
+            //Act
+            mainPageViewModel.Lookup.Execute(null);
+
+            //Assert
+            Assert.IsNotNullOrEmpty(mainPageViewModel.Definition);
+
+        }
+    }
+}
---
+++ Ifkam.Viewmodels.Tests/MainPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Input;
+
+namespace Ifkam.Viewmodels.Tests
+{
+    class MainPageViewModel
+    {
+        private string _word;
+        private string _definition;
+
+        public string Word
+        {
+            get { return _word; }
+            set { _word = value; }
+        }
+
+        public string Definition
+        {
+            get { return _definition; }
+            set { _definition = value; }
+        }
+
+        public ICommand Lookup
+        {
+            get { return null; }
+        }
+    }
+}
