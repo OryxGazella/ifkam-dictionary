99da88181c3a2369d14b783ea370a5e231101a29
Frank Smith <oryx.gazella@gmail.com>
2012-09-19 23:40:38 +0200

    create a rails service, note this is simply the result of 'rails new dictionary'

---
+++ service/dictionary/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dictionary::Application.config.secret_token = '81572f0c9803d718399793531580c4f530784fe58f6c7a8722e058e5c0ad0907e1cdc8bf32b168c46d0501426b1b307e3254d76804609f27aa182ac01f169074'
